91495558a75cf8ab2821f9d77115d32145770333
phong phan <phongphanq089@gmail.com>
2025-01-15 22:53:54 +0700

    tạo trang resoure

--- src/app/(content)/layout.tsx
+++ src/app/(content)/layout.tsx
@@ -1,9 +1,28 @@
+import LayoutWrapper from '@/components/layout/LayoutWrapper'
+import Footer from '@/components/shared/Footer/Index'
+import Header from '@/components/shared/header/Header'
+import { footerQuery } from '@/lib/queries-data'
+import { sanityFetch } from '@/sanity/sanityFetch'
+import { SanityDocument } from 'next-sanity'
 import React from 'react'
 
-const layout = () => {
+const Layout = async ({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) => {
+  const footeNav = await sanityFetch<SanityDocument>({
+    query: footerQuery,
+  })
   return (
-    <div>layout</div>
+    <>
+      <Header />
+      <div className='min-h-screen flex flex-col'>
+        <main className='flex-1'> {children}</main>
+      </div>
+      <Footer footer={footeNav as SanityDocument} />
+    </>
   )
 }
 
-export default layout
+export default Layout
---
+++ src/app/(content)/resoures/page.tsx
@@ -0,0 +1,12 @@
+import LayoutWrapper from '@/components/layout/LayoutWrapper'
+import Header from '@/components/shared/header/Header'
+import { footerQuery } from '@/lib/queries-data'
+import { sanityFetch } from '@/sanity/sanityFetch'
+import { SanityDocument } from 'next-sanity'
+import React from 'react'
+
+const PageResoures = () => {
+  return <div>sgfdsgd</div>
+}
+
+export default PageResoures
