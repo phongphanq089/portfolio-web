9f9a42d4a8bc0dd5c69ec498d13481f06e31ebb1
phong phan <phongphanq089@gmail.com>
2025-03-24 14:29:32 +0700

    cập nhật lại site map

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -4,7 +4,9 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = 'https://phongphan.xyz'
   const locales = ['en', 'vi']
-  const routes = ['', '/component-ui', '/blogs', '/resoures', '/video']
+  // const routes = ['', '/component-ui', '/blogs', '/resoures', '/video']
+
+  const routes = ['', '/resoures']
 
   // Tạo danh sách các URL cho từng ngôn ngữ
   const sitemapEntries: MetadataRoute.Sitemap = []
